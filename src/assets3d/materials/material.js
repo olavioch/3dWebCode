0b049c5b0ddc1ea4fbcef4808a6765024fe4a398
olavo Argemiro <olavo.resende@raccoon.ag>
2021-12-15 23:07:58 -0300

    melhorando o codigo

--- src/assets3d/materials/material.js
+++ src/assets3d/materials/material.js
@@ -6,6 +6,8 @@ import './bola.jpg';
 import './metal.jpg'
 import './normalMetal.png';
 import '../cena/back.jpg';
+import './normalConcreto.png';
+import './concreto.jpg';
 
 const normal = new THREE.TextureLoader().load('modelnormal.jpg');
 const textura = new THREE.TextureLoader().load('model.jpg');
@@ -13,6 +15,8 @@ const bolaNormal = new THREE.TextureLoader().load('bolaNormal.png');
 const bola = new THREE.TextureLoader().load('bola.jpg');
 const metalNormal = new THREE.TextureLoader().load('normalMetal.png');
 const metal = new THREE.TextureLoader().load('metal.jpg');
+const concretoNormal = new THREE.TextureLoader().load('normalConcreto.png');
+const concreto= new THREE.TextureLoader().load('concreto.jpg');
 
 const textureReflection = new THREE.TextureLoader().load('back.jpg');
 textureReflection.encoding = THREE.sRGBEncoding;
@@ -26,8 +30,22 @@ const material3d = {
         bumpMap: normal,
         bumpScale: 0.003,
         envMap: textureReflection,
-        envMapIntensity: 2,
-        roughness: 0.4,
+        envMapIntensity: 1.5,
+        roughness: 0,
+        metalness: 0, 
+        flatShading: false
+    }
+}
+const materialConcreto = {
+    transparent: false,
+    mat:{
+        color: new THREE.Color(0xffffff),
+        map: concreto,
+        bumpMap: concretoNormal,
+        bumpScale: 0.003,
+        envMap: textureReflection,
+        envMapIntensity: 1,
+        roughness: 0.3,
         metalness: 0, 
         flatShading: false
     }
@@ -73,7 +91,7 @@ const material3dC = {
     }
 }
 
-export {material3d, material3dA, material3dB, material3dC};
+export {material3d, material3dA, material3dB, material3dC, materialConcreto};
 
 //link para geração de normalmaps:
 // https://cpetry.github.io/NormalMap-Online/
--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
-import Plane from "./assets3d/utils/Plane";
 import objeto from "./assets3d/utils/objeto";
 import "./style.css";
-import "./assets3d/objetos3d/bola.obj";
-import "./assets3d/objetos3d/bolaB.obj";
-import "./assets3d/objetos3d/bolaC.obj";
-import "./assets3d/objetos3d/model.obj";
 import luz from './assets3d/luz';
 import {camera} from './assets3d/camera/camera';
 import {cena, cenaColor, cenaBack} from './assets3d/cena/cena';
-import {material3d, material3dA, material3dB, material3dC} from "./assets3d/materials/material";
+import {material3d, material3dA, material3dB, material3dC, materialConcreto} from "./assets3d/materials/material";
+import {configModel, configBolaB, configBolaC, configBola, configCube} from "./assets3d/objetos3d/config";
 
 
 
 
+objeto('hero', configModel, material3d, luz, camera, cena);
 
-objeto('hero', 'model.obj', material3d, luz, camera, cena, true);
 
-
-objeto('3dA', 'bola.obj', material3dA, luz, camera, cenaBack, true);
-objeto('3dB', 'bolaB.obj', material3dB, luz, camera, cenaColor, true);
-objeto('3dC', 'bolaC.obj', material3dC, luz, camera, cenaBack, true);
+objeto('3dA', configBola, material3dA, luz, camera, cenaBack);
+objeto('3dB', configBolaB, material3dB, luz, camera, cenaColor);
+objeto('3dC', configBolaC, material3dC, luz, camera, cenaBack);
 
 
 //Plane('hero', material3d, luz, camera, cena, true);
